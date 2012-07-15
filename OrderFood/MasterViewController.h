5c3465c539faa43b65a2a1596cebeda5666e6905
German Giunta <germangiunta@german-giuntas-macbook-pro.local>
2012-07-14 23:47:23 -0300

    Initial Commit

---
+++ OrderFood/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  OrderFood
+//
+//  Created by German Giunta on 7/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
